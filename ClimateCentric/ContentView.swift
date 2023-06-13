f376be83cefe031bd7b54e2cc92603fb5e9986c6
Seyma <seykilic0808@gmail.com>
2023-06-13 14:33:21 -0500

    Added buttons to the interaction page

--- ClimateCentric/ContentView.swift
+++ ClimateCentric/ContentView.swift
@@ -8,22 +8,131 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State
+       var expanded: Bool = false
     var body: some View {
         ZStack {
-            Color(red: 0.2, green: 0.4, blue: 0.2)
+            NavigationStack {
+                       Text("Interaction Center")
+                       .font(.custom("title", size: 17))
+                           .toolbar {
+                               ToolbarItem(placement: .automatic) {
+                                   Menu("Menu") {
+                                       
+                                       Button("Motivation Corner")
+                                       {}
+                                       Button("Interaction Center")
+                                       {}
+                                       Button("Involvement Links")
+                                       {}
+                                       Button("Carbon Footprint")
+                                       {}
+                            
+                                   }
+                                
+                                  
+                               }
+                               ToolbarItem(placement: .cancellationAction) {
+                                  //Color(red: 0.9, green: 1.0, blue: 0.0)
+                                   Text("ClimateCentric")
+                                       .font(.custom("DIN Condensed", size: 19))
+                                       .foregroundColor(Color(red: 0.0, green: 0.44313725490196076, blue: 0.3254901960784314))
+                                   
+                                    
+                                      
+                               }
+                               
+                               
+                           }
+                           .navigationBarTitleDisplayMode(.inline)
+                
+                           .toolbarBackground(Color(red: 0.8901960784313725, green: 1.0, blue: 0.0), for: .navigationBar)
+                           .toolbarBackground(.visible, for: .navigationBar)
+                          
+            
+                Color(red: 0.0, green: 0.44313725490196076, blue: 0.3254901960784314)
                 .ignoresSafeArea()
-       
+              
                 
-            Menu("Menu") {
-                
-                Text("Motivation Corner")
-                Text("Interaction Center")
-                Text("Involvement Links")
-                Text("Carbon Footprint")
+            }
+            
+        
+            
+          
+            Section {
+                  Text("How to Talk About Climate Change")
+                    .font(.title2).bold()
+                    .position(x:200, y:100)
+                    .foregroundColor(Color(red: 0.96, green: 1.0, blue: 0.93))
+                Text("According to \"Global Warming's Six Americas\"")
+                    .font(.body)
+                  .position(x:200, y:130)
+                  .foregroundColor(Color(red: 0.96, green: 1.0, blue: 0.93))
+                  .multilineTextAlignment(.center)
+            }
+            
+           
+                VStack{
+                    Button("Alarmed"){
+                        VStack{
+                            Text("sdfsdf")
+                        }
+                        .frame(width: 350.0, height: 320.0)
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:105,y:260)
                     
-            }
-            .offset(x:150, y:-350)
-            }
+                    Button("Concerned"){
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:105,y:260)
+                    
+                    Button("Cautious"){
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:105,y:260)
+                    
+                    Button("Disengaged"){
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:295,y:-123)
+                    
+                    Button("Doubtful"){
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:295,y:-123)
+                    
+                    
+                    Button("Dismissive"){
+                    }
+                    .frame(width: 150.0, height: 120.0)
+                
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+                    .position(x:295,y:-123)
+                }
+               
+                
+            
+           
+            
+                
+
+        }
             
             
             }
